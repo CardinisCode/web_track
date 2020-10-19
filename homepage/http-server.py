9e5e1f7ac63986fea77030d98e8a35817dd30a90
Andi Folgado <ac.folgado@gmail.com>
2020-10-19 18:12:20 +0100

    Testing page looking better.
    Keen to submit these changes before I try to add these css changes to another page.

--- homepage/http-server.py
+++ homepage/http-server.py
@@ -1,3 +1,7 @@
+# This code is extracted from:
+# https://stackabuse.com/serving-files-with-pythons-simplehttpserver-module/
+# It enables me to run a HTTP server, so that I could see the output of my code as I added/adjusted the code. 
+
 import http.server
 import socketserver
 
