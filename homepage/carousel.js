b644b6b2029bb23bec666295ca1f5734121142ad
Andi Folgado <ac.folgado@gmail.com>
2020-11-03 15:24:13 +0000

    Update
    The 2 carousels work :)

--- homepage/carousel.js
+++ homepage/carousel.js
@@ -1,14 +1,28 @@
-('.carouselGameboardControls').carousel({
-    interval: 2
-  })
-  ('.carouselColourControls').carousel({
-    interval: 2
-  })
+// JS for Gameboard Carousel
 
-  $(".left").click(function(){
+$('.carouselGameboardControls').carousel({
+    interval: 2
+})
+
+$(".gameboard_carousel_left").click(function(){
     $("#carouselGameboardControls").carousel("prev");
-  });
+});
 
-  $(".right").click(function(){
+$(".gameboard_carousel_right").click(function(){
     $("#carouselGameboardControls").carousel("next");
-  });
+});
+
+// -------------------------------------------------
+// JS for Colouring-in Images Carousel
+
+$('.carouselColourControls').carousel({
+    interval: 2
+})
+
+$(".colour_carousel_left").click(function(){
+    $("#carouselColourControls").carousel("prev");
+});
+
+$(".colour_carousel_right").click(function(){
+    $("#carouselColourControls").carousel("next");
+});
